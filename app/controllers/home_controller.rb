f72e23ceb61916cbd6b102b6707b543c921ae0b5
Elson Otake <elsonotake@yahoo.com.br>
2023-01-05 21:22:54 -0300

    Add fetch API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,24 @@
 class HomeController < ApplicationController
+  require 'dotenv'
+  require 'rest-client'
+  require 'json'
+  Dotenv.load
+
   def index
+
   end
 
   def wallets_index
+    payload = {
+      module: 'account',
+      action: 'txlist',
+      address: '0xEc690940081E780ae3310C88eb3f4C75622988eC',
+      sort: 'asc',
+      apikey: ENV['API_KEY']
+    }
+    url = 'https://api.etherscan.io/api'
+    response = RestClient.get url, { params: payload }
+    render json: response
   end
 
   def wallets_show
