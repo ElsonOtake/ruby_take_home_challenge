6934210aeb03a5c0316dd1c2934218973714b780
Elson Otake <elsonotake@yahoo.com.br>
2023-01-09 16:07:25 -0300

    Add user model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,13 +1,18 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
+  test "should save valid User" do
+    user = User.new(email: 'take@home.com', password: 'secret')
+    assert user.save, "Saved the valid user"
+  end
+
   test "should not save User without email" do
-    user = User.new
+    user = User.new(password: 'secret')
     assert_not user.save, "Saved the user without a email"
   end
 
   test "should not save User without password" do
-    user = User.new
+    user = User.new(email: 'take@home.com')
     assert_not user.save, "Saved the user without a password"
   end
 end
