768c003616d8a85bf5aad32d1966883dfa19d6dc
Elson Otake <elsonotake@yahoo.com.br>
2023-01-09 13:26:57 -0300

    Update validation to one line of code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  validates :email, presence: true
-  validates :password, presence: true
+  validates :email, :password, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
